a1c7b4e15524ec3acfd0c10271bfeb5913687336
Jim Robinson <jscrobinson@gmail.com>
2020-03-23 10:49:33 +0000

    Terraform base

---
+++ infra/google-project.tf
@@ -0,0 +1,4 @@
+resource "google_project" "school" {
+  name       = var.project_name
+  project_id = var.project_id
+}
---
+++ infra/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name" {
+
+}
+
+variable "project_id" {
+    
+}
